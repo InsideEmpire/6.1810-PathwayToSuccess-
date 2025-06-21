3aa025e674ad66ab3f1ff8a1f57ddb2aae5ce058
Garrison <garrison_liu@outlook.com>
2025-06-21 11:46:12 +0800

    Update pgtbl (#3)
    * finish Speed up system calls
    
    * finish Print a page table
    
    * finish Detect which pages have been accessed

--- kernel/vm.c
+++ kernel/vm.c
@@ -273,6 +273,34 @@ uvmdealloc(pagetable_t pagetable, uint64 oldsz, uint64 newsz)
   return newsz;
 }
 
+void
+vmprint_rec(pagetable_t pagetable, int depth)
+{
+  for (int i = 0; i < 512; i++) {
+    pte_t pte = pagetable[i];
+    if (pte & PTE_V) {
+      uint64 pa = PTE2PA(pte);
+
+      for (int d = 0; d < depth; d++) {
+        printf(" ..");
+      }
+
+      printf("%d: pte %p pa %p\n", i, (void*)pte, (void*)pa);
+
+      if ((pte & (PTE_R | PTE_W | PTE_X)) == 0) {
+        vmprint_rec((pagetable_t)pa, depth + 1);
+      }
+    }
+  }
+}
+
+void
+vmprint(pagetable_t pagetable)
+{
+  printf("page table %p\n", pagetable);
+  vmprint_rec(pagetable, 1);
+}
+
 // Recursively free page-table pages.
 // All leaf mappings must already have been removed.
 void
